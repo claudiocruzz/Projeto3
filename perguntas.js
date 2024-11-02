0c189130c87fa7d0549072a8795b9c3dc7e1220a
claudiocruzz <0000113554069xsp@al.educacao.sp.gov.br>
2024-11-02 19:31:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi aproximado?',
+    'O valor de Pi aproximado é 3,14.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um adjetivo?',
+    'Um adjetivo é uma palavra que caracteriza ou descreve um substantivo.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é a principal fonte de energia para a Terra?',
+    'A principal fonte de energia para a Terra é o Sol.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Dom Pedro I?',
+    'Dom Pedro I foi o primeiro imperador do Brasil, conhecido por declarar a independência do país em 1822.'
+)
